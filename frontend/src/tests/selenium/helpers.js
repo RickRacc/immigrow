f520e5e15f358df06344815f995077a93194ce37
Rakesh Singh <rakesh.p.singh2030@gmail.com>
2025-11-12 12:33:29 -0600

    reverst helpers.js code but add updated chromium version in package.json

--- frontend/src/tests/selenium/helpers.js
+++ frontend/src/tests/selenium/helpers.js
@@ -4,18 +4,7 @@ import chrome from "selenium-webdriver/chrome.js";
 export async function newDriver() {
   const options = new chrome.Options()
     .addArguments("--headless=new", "--no-sandbox", "--disable-gpu", "--window-size=1280,800");
-
-  // Use system chromedriver in CI (matches system chromium version)
-  const service = new chrome.ServiceBuilder();
-  if (process.env.CI) {
-    service.setPath('/usr/bin/chromedriver');
-  }
-
-  return await new Builder()
-    .forBrowser("chrome")
-    .setChromeOptions(options)
-    .setChromeService(service)
-    .build();
+  return await new Builder().forBrowser("chrome").setChromeOptions(options).build();
 }
 
 export { By, until };
